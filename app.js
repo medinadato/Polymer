64637a9b2afd44ae00a77ac21755a634c5ef329e
Renato Medina <medina@mdnsolutions.com>
2015-04-27 12:42:24 +0000

    Added core list elements

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+
+
 document.addEventListener('polymer-ready', function() {
+	// auto-binding template
 	var app = document.querySelector('#app');
+
 	app.heading = 'My Cool App';
 
 	app.selected = 0;
@@ -8,5 +12,21 @@ document.addEventListener('polymer-ready', function() {
 	  if (detail.isSelected) {
 	    document.querySelector('#drawerPanel').closeDrawer();
 	  }
-	};	
+	};
+
+	// Working with core-list and faker data
+	app.data = generateContacts();
+
+	function generateContacts() {
+		var data = [];
+		for(var i = 0; i < 1000; i++) {
+			data.push({
+				name: faker.name.findName(),
+				avatar: faker.internet.avatar()
+			});
+		}
+		return data;
+	}
+
+
 })
